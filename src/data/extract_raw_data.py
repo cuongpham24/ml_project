747669518231971cec98a394d4ce7977419522b3
cuongpham24 <cuongpham.iec@gmail.com>
2024-07-05 14:06:12 -0500

    refactoring code

--- src/data/extract_raw_data.py
+++ src/data/extract_raw_data.py
@@ -1,12 +1,9 @@
+"""
+This function take the original files and extract the first 1000000 rows for each category
+"""
+
 import pandas as pd
-
-# Update this source folder appropriately 
-RAW_DATA_FOLDER = "../../data/raw"
-
-# Define feature columns 
-dataset = ["meta_Beauty_and_Personal_Care", "meta_Books", "meta_Home_and_Kitchen"]
-labels = ["personal_care", "book", "home"]
-column_selections = ["main_category", "title", "features"]
+from gobal_variables import *
 
 max_size = 1000000
 sample_size = 100000
@@ -15,20 +12,20 @@ sample_size = 100000
 df = pd.DataFrame(columns=["main_category", "title", "features"])
 
 # Loop through each dataset and extract the first 1 million rows
-for index in range(len(dataset)):
-    chunks = pd.read_json(path_or_buf=f"{RAW_DATA_FOLDER}/{dataset[index]}.jsonl", lines=True, chunksize=sample_size)
+for index in range(len(DATASET)):
+    chunks = pd.read_json(path_or_buf=f"{RAW_DATA_FOLDER}/{DATASET[index]}.jsonl", lines=True, chunksize=sample_size)
 
     for id, c in enumerate(chunks):
         # Join into a paraphraph 
         c["features"] = c["features"].apply(lambda x: " ".join(x))
         # Set consistant label
-        c["main_category"] = labels[index]
+        c["main_category"] = LABELS[index]
         # Join data snippet to df
-        df = pd.concat([df, c[column_selections]])
+        df = pd.concat([df, c[COLUMN_SELECTIONS]])
         # Stop at predefined number of iterations
         if id == int(max_size / sample_size) - 1:
             break
 
     # Store extracted data
-    df.to_parquet(f"../../data/raw/{labels[index]}.parquet", compression="gzip")
-    print(f"Succesfully extract {labels[index]}")
+    df.to_parquet(f"../../data/raw/{LABELS[index]}.parquet", compression="gzip")
+    print(f"Succesfully extract {LABELS[index]}")
---
+++ src/data/gobal_variables.py
@@ -0,0 +1,7 @@
+# Update this source folder appropriately 
+RAW_DATA_FOLDER = "../../data/raw"
+
+# Define feature columns 
+DATASET = ["meta_Beauty_and_Personal_Care", "meta_Books", "meta_Home_and_Kitchen"]
+LABELS = ["personal_care", "book", "home"]
+COLUMN_SELECTIONS = ["main_category", "title", "features"]
